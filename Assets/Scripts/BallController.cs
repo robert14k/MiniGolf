de65b463cbdff548993cf5eb4e1836ae84a9e51f
Adam Zastrow <31371255+azastrow39@users.noreply.github.com>
2021-02-26 17:16:30 -0600

    stuffs

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallControllers : MonoBehaviour
+public class BallController : MonoBehaviour
 {
     public float speed;
     private Rigidbody rib;
@@ -16,19 +16,50 @@ public class BallControllers : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        //rib.velocity = rib.velocity / .99;
+        if( transform.position.y < .06)
+        {
+            gameObject.transform.position = new Vector3(transform.position.x, .06f, transform.position.z);
+        }
     }
 
-    //public void OnCollisionEnter(Collision other)
+    //public void ontriggerenter(Collider other)
     //{
+    //    Debug.Log("here one");
+    //    //if (other.gameobject.comparetag("putter"))
+    //    //{
+    //    //    var magnitude = 5;
+    //    //    var force = transform.position - other.transform.position;
+
+    //    //    force.normalize();
+    //    //    rib.addforce(force * magnitude);
+    //    //}
     //    if (other.gameObject.CompareTag("putter"))
     //    {
-    //        var magnitude = 5;
-    //        var force = transform.position - other.transform.position;
+    //        Debug.Log("here two");
+    //        // calculate force vector
+    //        var forcex = transform.position.x - other.transform.position.x;
+    //        var forcez = transform.position.z - other.transform.position.z;
+    //        // normalize force vector to get direction only and trim magnitude
+    //        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(forcex, 0f, forcez));
 
-    //        force.Normalize();
-    //        rib.AddForce(force * magnitude);
     //    }
     //}
+
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("putter"))
+        {
+            //// calculate force vector
+            //var forcex = transform. - other.transform.position;
+            // normalize force vector to get direction only and trim magnitude
+            var parent = other.transform.parent.gameObject;
+
+            var direction = new Vector3(parent.transform.rotation.y + 90, 0, parent.transform.rotation.y) ;
+            rib.AddForce(direction);
+
+        }
+    }
 
 }
